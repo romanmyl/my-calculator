36b647a977e6acc69b5ca713263433b7b76f7e87
Roman Mylenkyi <i.ua4@i.ua>
2017-01-27 22:54:33 +0000

    change design

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ function math(i) {
     var screen = document.getElementById("screen");
     if (screen.value == 0 || screen.value == "Error") {
         screen.value = i;
-        document.getElementById("memtab").value += i;
+        document.getElementById("memtab").innerHTML += i;
     } else {
      screen.value += i;
-     document.getElementById("memtab").value += i;
+     document.getElementById("memtab").innerHTML += i;
 }; 
 }
 
