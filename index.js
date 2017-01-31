d4849d818fdf0efb1cbecde6092b269fc292b6c1
Roman Mylenkyi <i.ua4@i.ua>
2017-01-31 21:00:11 +0000

    ake changes in js and so on

--- index.js
+++ index.js
@@ -1,28 +1,105 @@
+
 function math(i) {
     var screen = document.getElementById("screen");
-    if (screen.value == 0 || screen.value == "Error") {
-        screen.value = i;
-        document.getElementById("memtab").innerHTML += i;
+    var memtab = document.getElementById("memtab");
+    if (screen.innerHTML == "0" || screen.innerHTML == "Error" || screen.innerHTML.includes("=")) {
+        screen.innerHTML = i;
+        memtab.innerHTML += i;
     } else {
-     screen.value += i;
-     document.getElementById("memtab").innerHTML += i;
+     screen.innerHTML += i;
+     memtab.innerHTML += i;
 }; 
 }
 
-function c(z) {
-    return document.getElementById ("screen").value = z;
+function dot(d) {
+    var screen = document.getElementById("screen");
+    var memtab = document.getElementById("memtab");
+    if (screen.innerHTML.endsWith(d) || screen.innerHTML == "Error" || screen.innerHTML.includes("=")) {
+        screen.innerHTML = "";
+        memtab.innerHTML = "";
+    } else {
+        screen.innerHTML += d;
+        memtab.innerHTML += d;
+    };
 }
 
-function equal () {
-    
-    try {
-    
-    document.getElementById ("screen").value = eval(document.getElementById ("screen").value);
-} catch (error) {
-    document.getElementById ("screen").value = "Error";
+function operator(opr) {
+    var screen = document.getElementById("screen");
+    var memtab = document.getElementById("memtab");
+    var op = "<span>" + opr + "</span>";
+    if (screen.innerHTML != 0 && screen.innerHTML != "Error" && screen.innerHTML.includes("=") == false) {
+        screen.innerHTML += op;
+        memtab.innerHTML += op;
+    } ;
+}
+
+function operMore(opr) {
+    var screen = document.getElementById("screen");
+    var memtab = document.getElementById("memtab");
+    var op = "<span>" + opr + "</span>";
+    if ( screen.innerHTML == "0" || screen.innerHTML == "Error" || screen.innerHTML.includes("=")) {
+       screen.innerHTML = op;
+       memtab.innerHTML += op;
+   } else {
+        screen.innerHTML += op;
+        memtab.innerHTML += op;
+    } ;
+}
+
+function c(z) {    
+    var screen = document.getElementById("screen");
+    var memtab = document.getElementById("memtab");
+    if (memtab.lastChild.nodeName == "BR") {
+    screen.innerHTML = z;
+    memtab.innerHTML += "";
+   } else {
+    screen.innerHTML = z;
+    memtab.innerHTML +="<br><br>";
 };
 }
 
+function evl() {
+    var screen = document.getElementById("screen");
+    var memtab = document.getElementById("memtab");
+    try {
+        if (screen.innerHTML.includes("=") || screen.innerHTML.includes("Error") || screen.innerHTML == 0) {
+            screen.innerHTML += "";
+            memtab.innerHTML += "";
+        } else {
+        var getText = screen.textContent; 
+        getText = getText.replace( /×/g, "*");
+        getText = getText.replace( /÷/g, "/");
+        getText = getText.replace( /√/g, "Math.sqrt");
+
+        var result = eval(getText);
+        if (result.toString().length >=10) {
+            result = result.toExponential(5);
+    screen.innerHTML += "<br>" + "<span class='result'>=" + result + "</span>";
+    memtab.innerHTML += "<br>" +  "<span class='result'>=" + result + "</span>" + "<br><br>";
+        } else {
+    screen.innerHTML += "<br>" + "<span class='result'>=" + result + "</span>";
+    memtab.innerHTML += "<br>" +  "<span class='result'>=" + result + "</span>" + "<br><br>";
+}; 
+            
+        };
+} catch(error) {
+    screen.innerHTML = "Error";
+   memtab.innerHTML += "<br><br>";
+}; 
+}
+
 function ch() {
-    document.getElementById ("memtab").innerHTML = "";
+    var memtab = document.getElementById("memtab");
+    memtab.innerHTML = "";
+}
+
+function  del() {
+    var screen = document.getElementById("screen");
+    var memtab = document.getElementById("memtab");
+    if (screen.innerHTML == "Error" || screen.innerHTML == 0) {
+        screen.innerHTML = 0;
+    } else {
+    screen.innerHTML = screen.innerHTML.substr(0, screen.innerHTML.length-1);
+    memtab.innerHTML = memtab.innerHTML.substr(0, memtab.innerHTML.length-1);
+};
 }
